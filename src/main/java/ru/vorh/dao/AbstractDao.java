3be64a113d2e212fa1b0efec6904502e8c0e19ca
vorh <sorcerer12@list.ru>
2017-04-10 10:56:59 +0300

    ADD [ Abstract Dao, Random Dao ]

---
+++ src/main/java/ru/vorh/dao/AbstractDao.java
@@ -0,0 +1,22 @@
+package ru.vorh.dao;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * Created by vorh on 4/10/17.
+ */
+public class AbstractDao {
+
+    private DriverManagerDataSource dataSource;
+
+    public AbstractDao() {
+        dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/sundries");
+    }
+
+    public NamedParameterJdbcTemplate getNpjt(){
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+}
---
+++ src/main/java/ru/vorh/dao/RandomDao.java
@@ -0,0 +1,10 @@
+package ru.vorh.dao;
+
+/**
+ * Created by vorh on 4/10/17.
+ */
+public class RandomDao extends AbstractDao{
+
+
+
+}
